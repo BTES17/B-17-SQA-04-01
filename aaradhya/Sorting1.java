c8aeef3361e107c60ee622c2d57dafad365cd539
BTES17 <ramandeep@btes.co.in>
2017-05-03 13:51:35 +0530

    Add files via upload

---
+++ aaradhya/Sorting1.java
@@ -0,0 +1,35 @@
+package aaradhya2;
+
+import java.util.Scanner;
+
+public class Sorting1 {
+public static void main(String[] args){
+	int i,j,temp;
+	System.out.println("enter size");
+	Scanner input=new Scanner(System.in);
+	int size=input.nextInt();
+	int[] a=new int[size];
+	System.out.println("enter values");
+	for(i=0;i<size;i++)
+	{
+		a[i]=input.nextInt();
+	}
+	for(j=0;j<size-1;j++)
+	{
+		for(i=j+1;i<size;i++)
+		{
+		if(a[j]>a[i])
+		{
+			temp=a[j];
+			a[j]=a[i];
+			a[i]=temp;
+			
+		}
+		}}
+	for(i=0;i<size;i++)
+	{
+		System.out.println(a[i]);
+	
+}
+}
+}

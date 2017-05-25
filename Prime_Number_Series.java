ab5fbb01bfcba5aa14c108f73a5e5c0241972499
BTES17 <ramandeep@btes.co.in>
2017-05-25 16:17:17 +0530

    Add files via upload

---
+++ Prime_Number_Series.java
@@ -0,0 +1,27 @@
+package Mypackage;
+
+import java.util.Scanner;
+
+public class Prime_Number_Series {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num,p;
+		System.out.println("Enter upto which number prime numbers are needed - ");
+		Scanner s=new Scanner(System.in);		
+		num=s.nextInt();
+		System.out.println("Here are the Prime number's up to " +num+ " number - ");
+		for(int i=1;i<=num;i++)
+		{
+			p=0;
+			for(int j=2;j<i;j++)
+			{
+				if(i%j==0)
+				p=1;
+			}
+		if(p==0)
+			System.out.println(+i);
+		}
+	}
+
+}

7bd8d3127165ba0c95c997467486fdad344801de
BTES17 <ramandeep@btes.co.in>
2017-05-30 19:14:07 +0530

    String(Use of methods like toUpperCase, toLowerCase, contains, equals, charAt and lenght)

---
+++ Gaurav Program/String_Sample.java
@@ -0,0 +1,46 @@
+package Mypackage;
+
+public class String_Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//Use of Equal method
+String ch = "My name is java.";
+System.out.println("A. Use of equals method");
+	boolean A = ch.equals("My name is Java.");
+	System.out.println(A);
+	//Use of Length method
+System.out.println("*************************");
+System.out.println("B. Use of length method");
+	System.out.print("Number of characher are ");
+		int B = ch.length();
+		System.out.println(B);
+	//Use of charAt method
+int Reverse = B;
+System.out.println("*************************");
+System.out.println("C. Use of charAt method");
+	System.out.print("Reverse of entered sentence are '");
+		while(Reverse > 0)
+			{
+				System.out.print(ch.charAt(Reverse-1));
+				Reverse--;
+			}
+		System.out.print("'");
+		System.out.println();
+	//Use of toLowerCase method
+System.out.println("*************************");
+System.out.println("D. Use of toLowerCase method");
+	String lowercase = ch.toLowerCase();
+	System.out.println(lowercase);
+	//Use of toUpperCase method
+System.out.println("*************************");
+System.out.println("E. Use of toUpperCase method");
+	String uppercase = ch.toUpperCase();
+	System.out.println(uppercase);
+	//Use of contains method
+System.out.println("*************************");
+System.out.println("F. Use of contains method");
+	System.out.println(ch.contains("java"));
+	}
+
+}
